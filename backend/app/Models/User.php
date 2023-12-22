07c54176a8d46fd91007d4f26e3316dca2bd8257
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-22 21:25:14 +0200

    add fillable to user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
+        'phone',
         'password',
+        'user_type',
     ];
 
     /**
