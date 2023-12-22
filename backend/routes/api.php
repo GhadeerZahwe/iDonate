109ec7e5ef173ae6878a037ad9bcc6480df317ab
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-22 23:35:24 +0200

    Backend: add logot route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,3 +18,4 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, "register"]);
 Route::post('/login', [AuthController::class, "login"]);
+Route::post('/logout', [AuthController::class, "logout"]);
