5de34aa4d28746862a7db128432ff86500133d08
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-23 10:26:20 +0200

    Backend: Get All Donors

--- backend/app/Http/Controllers/DonorController.php
+++ backend/app/Http/Controllers/DonorController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\DonorInfo;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 
 class DonorController extends Controller
 {
-    
+    public function getAllDonors(){
+        $donors=User::where('user_type','donor')
+        ->join('donors_info','users.id','=','donors_info.donor_id')
+        ->get();
+
+        return response()->json([
+            'donors'=>$donors
+        ]);
+    }
 }
