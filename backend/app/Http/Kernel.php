fb76c84abcc9bd7edc686fde3723bec165b287d2
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-24 12:33:18 +0200

    Backend: R

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http;
-
+use App\Http\Middleware\AuthenticateUser;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -54,7 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        // 'idonate_authenticate'=>AuthenticateUser::class,
+        'idonate_authenticate'=>AuthenticateUser::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
