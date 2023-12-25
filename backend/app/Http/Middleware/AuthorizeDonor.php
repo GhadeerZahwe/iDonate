307e79f59296e64d0d473eaa166c6cbd5e346b99
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 22:36:37 +0200

    Backend: Authorize Donor Middleware :stuck_out_tongue_closed_eyes:

--- backend/app/Http/Middleware/AuthorizeDonor.php
+++ backend/app/Http/Middleware/AuthorizeDonor.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizeDonor
@@ -15,6 +16,10 @@ class AuthorizeDonor
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $user=Auth::user();
+        if($user->user_type=== 'donor'){
+            return $next($request);
+        }
+        return response()->json(['error'=>'Not authorized as a Donor.']);
     }
 }
