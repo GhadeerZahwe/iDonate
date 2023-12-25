44346d41d4f65735f194a1be9a3dd55e71fcbe9d
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 17:49:55 +0200

    Backend: Add Get All Donors to Admin Controller :flushed:

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    
+     public function getAllDonors(){
+     if(auth()->check()){
+       
+        $user=Auth::user();
+
+       if($user->user_type==='admin'){
+        $donors=User::withTrashed()
+        ->where('user_type','donor')
+        ->join('donors_info','users.id','=','donors_info.donor_id')
+        ->get();
+
+        return response()->json(['donors'=> $donors]);
+
+        } else{
+        return response()->json(['error'=>'Permission Denied'],403);
+    }        
+     } else{
+        return response()->json(['error'=>'User not authenticated'],401);
+    }  
+  } 
 }
