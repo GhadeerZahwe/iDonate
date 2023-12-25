43b62ce6314b48c008912af8052d618671ea9c4c
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 22:24:45 +0200

    Backend: Add Authorize Admin Middleware to Kernel.php :smiley:

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 use App\Http\Middleware\AuthenticateUser;
+use App\Http\Middleware\AuthorizeAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -55,6 +56,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'idonate_authenticate'=>AuthenticateUser::class,
+        'admin_authorize'=>AuthorizeAdmin::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
