5a3844df7f194ca465f9666edb8ec524d3214f76
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 17:59:36 +0200

    Backend: Add Get All Deliveries to Admin Controller :flushed:

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\DeliveryInfo;
+use App\Models\DonorInfo;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -28,4 +29,30 @@ class AdminController extends Controller
         return response()->json(['error'=>'User not authenticated'],401);
     }  
   } 
+
+  public function getAllDeliveries()
+    {
+        try {
+            // Check if the user is authenticated
+            if (auth()->check()) {
+                $user = Auth::user();
+
+                // Check if the user is an admin
+                if ($user->user_type === 'admin') {
+                    // Get all delivery users with their delivery info
+                    $deliveries = User::where('user_type', 'delivery')
+                        ->with('deliveryInfo') 
+                        ->get();
+
+                    return response()->json(['deliveries' => $deliveries]);
+                } else {
+                    return response()->json(['error' => 'Permission Denied'], 403);
+                }
+            } else {
+                return response()->json(['error' => 'User not authenticated'], 401);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
