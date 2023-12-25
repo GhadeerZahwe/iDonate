70e306b2888251b0f4402b486cbc55d01aea6288
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 16:38:24 +0200

    Backend: Add Admin Controller :woozy_face:

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DonorController;
-
 use App\Http\Controllers\DeliveryController;
 
 /*
