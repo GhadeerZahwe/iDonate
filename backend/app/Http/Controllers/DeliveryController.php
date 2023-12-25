5279cdabfef551e6aa5044d2547f7ac9241f4daa
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 17:57:14 +0200

    Backend: Remove Get All Deliveries from Delivery Controller :flushed:

--- backend/app/Http/Controllers/DeliveryController.php
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -8,29 +8,5 @@ use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
 {
-    public function getAllDeliveries()
-    {
-        try {
-            // Check if the user is authenticated
-            if (auth()->check()) {
-                $user = Auth::user();
-
-                // Check if the user is an admin
-                if ($user->user_type === 'admin') {
-                    // Get all delivery users with their delivery info
-                    $deliveries = User::where('user_type', 'delivery')
-                        ->with('deliveryInfo') 
-                        ->get();
-
-                    return response()->json(['deliveries' => $deliveries]);
-                } else {
-                    return response()->json(['error' => 'Permission Denied'], 403);
-                }
-            } else {
-                return response()->json(['error' => 'User not authenticated'], 401);
-            }
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
-    }
+    
 }
