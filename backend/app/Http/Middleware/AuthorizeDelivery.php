74cb40313eed149eb56c6896207600dc59a3ae94
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 22:49:48 +0200

    Backend: Authorize Delivery Middleware :sunglasses:

--- backend/app/Http/Middleware/AuthorizeDelivery.php
+++ backend/app/Http/Middleware/AuthorizeDelivery.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Closure;
 use Illuminate\Http\Request;
+use App\Models\DeliveryInfo;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizeDelivery
@@ -15,6 +16,11 @@ class AuthorizeDelivery
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $user=Auth::user();
+        $delivery=DeliveryInfo::where('delivery_id',$user->id)->first();
+        if($user->user_type === 'delivery' and $delivery->is_approved===1){
+            return $next($request);
+        }
+        return response()->json(['error'=>'Not authorized as a delivery.']);
     }
 }
