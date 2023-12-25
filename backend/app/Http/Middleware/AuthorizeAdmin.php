2c60c435942d387aa0de973af97f0cd71b65c985
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 22:21:38 +0200

    Backend:  Admin Middleware :wink:

--- backend/app/Http/Middleware/AuthorizeAdmin.php
+++ backend/app/Http/Middleware/AuthorizeAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthorizeAdmin
@@ -15,6 +16,10 @@ class AuthorizeAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        $user=Auth::user();
+        if($user->user_type==='admin'){
+            return $next($request);
+        }
+        return response()->json(['error'=>'Not authorized as an Admin.']);
     }
 }
