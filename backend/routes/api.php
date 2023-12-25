92838f00db1d1473cc2f4485264eac7578921299
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-25 16:36:28 +0200

    Backend: Add route for Get All Deliveries :exploding_head:

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DonorController;
+
 use App\Http\Controllers\DeliveryController;
 
 /*
