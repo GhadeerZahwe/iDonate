3773413b619741bf4598314bf2d500a63d05c964
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 00:53:35 +0200

    Electron: Admin Header :innocent:

--- electron/src/components/admin_header/index.jsx
+++ electron/src/components/admin_header/index.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import UseHttp from "../../hooks/http-hook";
+import { useNavigate } from "react-router-dom";
 
 const AdminHeader = () => {
+  const handleLogoutClick = async () => {
+    const data = await UseHttp("logout", "POST", "", {
+      Authorization: "Bearer" + localStorage.getItem("token"),
+    });
+    console.log(data);
+    if (data.status === "success") {
+    }
+  };
   return (
     <div className="container">
       <div className="navbar">
