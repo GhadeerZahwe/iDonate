1e63f709521b469e86e470e817b47ef32f3f4d38
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 00:30:29 +0200

    Electron:Use Http :innocent:

--- electron/src/hooks/http-hook.js
+++ electron/src/hooks/http-hook.js
@@ -2,8 +2,19 @@ const UseHttp = async (url, method = "GET", body = "", header = {}) => {
   let data;
   const URL = "http://localhost:8000/api/";
   try {
-  } catch (err) {
-    console.log(err.message);
+    const Response =
+      (await fetch(URL + url, {
+        method: method,
+        headers: header,
+        body: !!body ? body : null,
+      })) || null;
+    data = await Response.json();
+    if (!Response.ok) {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.error(error.message);
+    throw error;
   }
   return data;
 };
