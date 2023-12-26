ea333a1eef99edf65566ff4e981624644b898bf5
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 00:54:16 +0200

    Electron: Add Navigate :woozy_face:

--- electron/src/components/admin_header/index.jsx
+++ electron/src/components/admin_header/index.jsx
@@ -3,12 +3,14 @@ import UseHttp from "../../hooks/http-hook";
 import { useNavigate } from "react-router-dom";
 
 const AdminHeader = () => {
+  const navigate = useNavigate();
   const handleLogoutClick = async () => {
     const data = await UseHttp("logout", "POST", "", {
       Authorization: "Bearer" + localStorage.getItem("token"),
     });
     console.log(data);
     if (data.status === "success") {
+      navigate("/");
     }
   };
   return (
