dffa83797be51cc928a74231a1c8c795fa514a5d
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:52:00 +0200

    Electron: Import Drivers page :car:

--- electron/src/App.js
+++ electron/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login/login";
 import HomePage from "./pages/HomePage/index";
+import Drivers from "./pages/Drivers/Drivers";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/admin_panel" element={<HomePage />} />
+      <Route path="" element={<Drivers />} />
     </Routes>
   );
 }
