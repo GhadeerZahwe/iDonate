3adeccb157a09838877b4486a30260e33429d152
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 08:42:20 +0200

    Electron: Table Header :fishsticks:

--- electron/src/components/DeliveriesTable/DeliveriesTable.jsx
+++ electron/src/components/DeliveriesTable/DeliveriesTable.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react";
+import Tr from "../Deliveries/Deliveries";
 
 const DeliveriesTable = () => {
   return (
     <div className="table-container">
-      <table></table>
+      <table>
+        <thead id="thead">
+          <tr>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Mobility Type</th>
+            <th>License Number</th>
+            <th></th>
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 };
