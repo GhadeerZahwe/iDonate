f4892305998da48e074759fbfdf967ae9153d00e
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 07:18:07 +0200

    Electron: Donors Delete :last_quarter_moon_with_face:

--- electron/src/components/Donors/donors.jsx
+++ electron/src/components/Donors/donors.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import UseHttp from "../../hooks/http-hook";
 
 const Tr = (props) => {
   const formData = new FormData();
   const deleteDonor = async (donor_id) => {
     formData.append("id", donor_id);
+    const data = await UseHttp("delete_user", "POST", formData, {
+      Authorization: "Bearer" + localStorage.getItem("token"),
+    });
   };
   return (
     <tr>
