3a3318d0550a640e2ad85a061352dcc2530278e1
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:19:24 +0200

    Electron: Import Components in Drivers Page Component :weary:

--- electron/src/pages/Drivers/Drivers.jsx
+++ electron/src/pages/Drivers/Drivers.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import AdminSidebar from "../../components/admin_sidebar";
+import AdminHeader from "../../components/admin_header";
 
 const Drivers = () => {
-  return <div>Drivers</div>;
+  return (
+    <body>
+      <AdminSidebar />
+      <AdminHeader />
+      <div></div>
+    </body>
+  );
 };
 
 export default Drivers;
