68ca288eacea215a76e1aabd02d2247af1954e2e
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:49:46 +0200

    Electron: Import Home Page and Adding its Route :octopus:

--- electron/src/App.js
+++ electron/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/login/login";
+import HomePage from "./pages/HomePage/index";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/admin_panel" element={<HomePage />} />
     </Routes>
   );
 }
