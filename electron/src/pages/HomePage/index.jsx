6ee3886721e821c0deedd2229d6a8c881878ff1d
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:05:58 +0200

    Electron: Import Users table Component :accessibility:

--- electron/src/pages/HomePage/index.jsx
+++ electron/src/pages/HomePage/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import AdminSidebar from "../../components/admin_sidebar";
 import AdminHeader from "../../components/admin_header";
+import UsersTable from "../../components/users_table";
 
 const HomePage = () => {
   return (
     <body>
       <AdminSidebar />
       <AdminHeader />
+      <div className="container2">
+        <div className="content">
+          <h1 className="content-title" id="content-title">
+            Donors
+          </h1>
+          <UsersTable />
+        </div>
+      </div>
     </body>
   );
 };
