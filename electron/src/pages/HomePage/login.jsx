90ba994dc332f33172e7af96dc5a39a3ae9c45c9
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:12:37 +0200

    Electron: Import Login Component :performing_arts:

--- electron/src/pages/HomePage/login.jsx
+++ electron/src/pages/HomePage/login.jsx
@@ -1,9 +1,7 @@
-import React from "react";
+import { Login1 } from "../../components/login/login";
 
 const Login = () => {
-  return (
-    <Login
-  );
+  return <Login1 />;
 };
 
 export default Login;
