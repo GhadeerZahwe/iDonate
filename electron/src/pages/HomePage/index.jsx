5c5f887da39f1aaef39ecdc51965edcaad75237d
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:03:21 +0200

    Electron: Add Components HomePage :bricks:

--- electron/src/pages/HomePage/index.jsx
+++ electron/src/pages/HomePage/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import AdminSidebar from "../../components/admin_sidebar";
+import AdminHeader from "../../components/admin_header";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <body>
+      <AdminSidebar />
+      <AdminHeader />
+    </body>
+  );
 };
 
 export default HomePage;
