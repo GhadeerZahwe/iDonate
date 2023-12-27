cd5659a64f1c1c5be0b4ed12cf10196f6e07f8f9
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 06:51:33 +0200

    Electron: Admin SideBar :woman_shrugging:

--- electron/src/components/admin_sidebar/index.jsx
+++ electron/src/components/admin_sidebar/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const AdminSidebar = () => {
+  return <div>AdminSidebar</div>;
 };
 
-export default index;
+export default AdminSidebar;
