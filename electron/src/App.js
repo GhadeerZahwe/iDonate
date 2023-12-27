68e985dd10c328e44dbe895e19376a9c18636556
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:48:19 +0200

    Electron: Adding login Page Route :cherry_blossom:

--- electron/src/App.js
+++ electron/src/App.js
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/login/login";
 function App() {
   return (
     <Routes>
-      <Route />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
