f557b8fe9c6e59ca6cf178e4c76e7314d40b9654
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 07:00:22 +0200

    Electron: Admin SideBar Donors :man_dancing:

--- electron/src/components/admin_sidebar/index.jsx
+++ electron/src/components/admin_sidebar/index.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "./logo.png";
 
 const AdminSidebar = () => {
-  return <div>AdminSidebar</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="side-panel">
+      <img className="logo" src={logo} alt="logo"></img>
+      <div className="donor-btn" id="users-btn" onClick={goDonors}>
+        Donors
+      </div>
+    </div>
+  );
 };
 
 export default AdminSidebar;
