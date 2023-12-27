80c9d1c9583e894afdaf6be677f6685ef765ba38
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 18:47:14 +0200

    Electron: Adding Router :bouquet:

--- electron/src/App.js
+++ electron/src/App.js
@@ -1,6 +1,11 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route />
+    </Routes>
+  );
 }
 
 export default App;
