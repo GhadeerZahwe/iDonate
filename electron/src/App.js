d154b189762d69dbfe6a943fa03bde2c4d8e2ace
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 19:58:37 +0200

    Electron: Fixes :thread:

--- electron/src/App.js
+++ electron/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Login from "./components/login/login";
 import HomePage from "./pages/HomePage/index";
 import Drivers from "./pages/Drivers/Drivers";
+import Donations from "./pages/Donations/Donations";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/admin_panel" element={<HomePage />} />
       <Route path="/admin_panel/drivers" element={<Drivers />} />
+      <Route path="/admin_panel/donations" element={<Donations />} />
     </Routes>
   );
 }
---
+++ electron/src/pages/Donations/Donations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Donations = () => {
+  return <div>Donations</div>;
+};
+
+export default Donations;
