572b48944ab8761358c10ca19811dc6a41fe0801
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-27 07:48:52 +0200

    Electron: Delete Delivery Driver :motor_scooter:

--- electron/src/components/Deliveries/Deliveries.jsx
+++ electron/src/components/Deliveries/Deliveries.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import UseHttp from "../../hooks/http-hook";
 
-const Tr = () => {
-  return <div>Tr</div>;
+const Tr = (props) => {
+  console.log(props);
+  const formData = new FormData();
+  const deleteDriver = async (delivery_id) => {
+    formData.append("id", delivery_id);
+    const data = await UseHttp("delete_user", "POST", formData, {
+      Authorization: "Bearer" + localStorage.getItem("token"),
+    });
+  };
+  return <div></div>;
 };
 
 export default Tr;
