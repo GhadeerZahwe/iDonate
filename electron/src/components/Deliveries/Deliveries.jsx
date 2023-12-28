aaa080a9f37de400b59d550117e3eb42d8d9d0c0
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-28 16:48:17 +0200

    Electron: Delivery Buttons Display Based on Status :speak_no_evil:

--- electron/src/components/Deliveries/Deliveries.jsx
+++ electron/src/components/Deliveries/Deliveries.jsx
@@ -1,23 +1,55 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from "react";
+import "./style.css"; // Import the CSS file
 import UseHttp from "../../hooks/http-hook";
 
 const Tr = (props) => {
-  console.log(props);
   const formData = new FormData();
-  const deleteDriver = async (delivery_id) => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [isApproved, setIsApproved] = useState(
+    props.data.delivery_info.is_approved
+  );
+
+  const handleButtonClick = async (delivery_id, is_approved) => {
     formData.append("id", delivery_id);
-    const data = await UseHttp("delete_user", "POST", formData, {
-      Authorization: "Bearer" + localStorage.getItem("token"),
-    });
+
+    try {
+      setIsProcessing(true);
+
+      const data = await UseHttp(
+        is_approved === 0
+          ? `acceptDelivery/${delivery_id}`
+          : `cancelDeliveryAcceptance/${delivery_id}`,
+        "POST",
+        formData,
+        {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        }
+      );
+
+      if (data.success) {
+        console.log(
+          is_approved === 0 ? "Approval successful" : "Cancellation successful"
+        );
+
+        // Update the state after successful API call
+        setIsApproved(is_approved === 0 ? 1 : 0);
+
+        // trigger a re-fetch of data or update the UI as needed
+      } else {
+        console.error(
+          is_approved === 0 ? "Approval failed:" : "Cancellation failed:",
+          data.error
+        );
+        // Handle the error, show a message, etc.
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+      // Handle other errors if necessary
+    } finally {
+      setIsProcessing(false);
+    }
   };
 
-  const approveDriver = async (delivery_id) => {
-    formData.append("id", delivery_id);
-    const data = await UseHttp("acceptDelivery", "POST", formData, {
-      Authorization: "Bearer" + localStorage.getItem("token"),
-    });
-  };
   return (
     <tr>
       <td>{props.data.first_name}</td>
@@ -28,20 +60,32 @@ const Tr = (props) => {
       <td>{props.data.delivery_info.license_number}</td>
 
       <td className="btn-container">
-        <div
-          className="delete_btn"
-          onClick={() => deleteDriver(props.data.delivery_id)}
-        >
-          Delete
-        </div>
-        {props.data.is_approved === 0 ? (
+        {isProcessing ? (
+          <div>Processing...</div>
+        ) : (
           <div
-            className="approve_btn"
-            onClick={() => approveDriver(props.data.delivery_id)}
+            className={isApproved ? "btn-delete" : "btn-approve"}
+            onClick={() =>
+              handleButtonClick(
+                props.data.delivery_info.delivery_id,
+                props.data.delivery_info.is_approved
+              )
+            }
           >
-            Approve
+            <strong>{isApproved ? "CANCEL" : "APPROVE"}</strong>
+            <div id={`container-stars-${isApproved ? "delete" : "approve"}`}>
+              <div id={`stars-${isApproved ? "delete" : "approve"}`}></div>
+            </div>
+            <div id={`glow-${isApproved ? "delete" : "approve"}`}>
+              <div
+                className={`circle-${isApproved ? "delete" : "approve"}`}
+              ></div>
+              <div
+                className={`circle-${isApproved ? "delete" : "approve"}`}
+              ></div>
+            </div>
           </div>
-        ) : null}
+        )}
       </td>
     </tr>
   );
