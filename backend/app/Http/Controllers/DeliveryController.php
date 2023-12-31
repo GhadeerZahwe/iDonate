39371c03105acc14df857908aebe572de40b2650
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-01 01:38:58 +0200

    Backend: Remove Get Delivery Donations :yawning_face:

--- backend/app/Http/Controllers/DeliveryController.php
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -12,42 +12,6 @@ use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
 {
-    public function getDeliveryDonations(Request $request){
-        $user=Auth::user();
-        if($user->user_type === 'delivery'){
-            try{
-              $pendingDonations=$this->getDonationsByStatus($user,'pending');
-              $onTheWayDonations=$this->getDonationsByStatus($user,'on_the_way');
-              $receivedDonations=$this->getDonationsByStatus($user,'received');
+   
 
-              return response()->json([
-                'Pending_Donations'=>$pendingDonations,
-                'On_The_Way_Donations'=>$onTheWayDonations,
-                'Completed_Donations'=>$receivedDonations,
-              ]);
-
-            }catch(\Exception $e){
-                return response()->json(['error' => $e->getMessage()], 500);
-            }
-        }else{
-            return response()->json(['error' => 'Permission Denied'], 403);
-        }
-    }
-
-    private function getDonationsByStatus($user, $status)
-    {
-        $donations = Order::where('delivery_id', $user->id)
-            ->where('status', $status)
-            ->with(['donor', 'orderItems', 'locations'])
-            ->get();
-
-        $formattedDonations = $donations->map(function ($donation) {
-            return $donation;
-        });
-    
-        return $formattedDonations;
-    }
-
-    
-    
 }
