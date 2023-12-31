0c5383dd04c734516a2b4ae534dac341af2d15da
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-31 21:23:48 +0200

    Backend: Import Order Model :relaxed:

--- backend/app/Http/Controllers/DeliveryController.php
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Models\DeliveryInfo;
 use App\Models\User;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
