f8a40786694e6394cf281f0ba43f69b8c6dacffd
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-01 20:18:54 +0200

    Frontend: Search Component :monocle_face:

---
+++ frontend/components/Search/Search.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, TextInput } from "react-native";
+
+const Search = () => {
+  return (
+    <TextInput
+      style={{
+        height: 40,
+        top: 400,
+      }}
+      placeholder="Search"
+    />
+  );
+};
+
+export default Search;
