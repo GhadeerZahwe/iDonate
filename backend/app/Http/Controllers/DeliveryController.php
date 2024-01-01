419f65b9af95ce5fbe66212f514b4b7f90c9899f
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-01 02:05:28 +0200

    Backend: Check Auth :mask:

--- backend/app/Http/Controllers/DeliveryController.php
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -14,6 +14,11 @@ class DeliveryController extends Controller
 {
    public function acceptOrder(Request $request, $orderId){
     try{
+     
+     $delivery=Auth::user;
+     if($delivery->user_type !== 'delivery'){
+        return response()->json(['error'=>'Permission Denied.'],403);
+     }
 
     }catch(\Exception $e){
         return response()->json(['error'=> $e->getMessage()],500);
