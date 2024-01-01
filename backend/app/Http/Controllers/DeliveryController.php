9a4c6f06c0510f4db1f3037bcacc107f2c672dcf
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-01 03:00:47 +0200

    Backend: Resest Status and Return Success Message :sparkles:

--- backend/app/Http/Controllers/DeliveryController.php
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -66,6 +66,13 @@ class DeliveryController extends Controller
             return response()->json(['error'=>'Order not found or cannot be canceled.'],404);
         }
 
+        $order->staus='pending';
+        $order->is_approved=false;
+        $order->delivery_id=null;
+        $order->save();
+
+        return response()->json(['Success_Message'=>'Order canceled Successfully.'],200);
+
         }catch(\Exception $e){
             return response()->json(['error'=> $e->getMessage()],500);
         }
