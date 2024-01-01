5dc3e6fb2c500c9613bbcdf8f75180289156e748
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-01 02:16:19 +0200

    Backend: Check total weight and mobility type :slightly_frowning_face:

--- backend/app/Http/Controllers/DeliveryController.php
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -30,6 +30,12 @@ class DeliveryController extends Controller
         return response()->json(['error'=>'Order not found or already assigned.'],404);
      }
 
+     $totalWeight= $order->total_weight;
+     if(($totalWeight>30 && $delivery->mobility_type=== 'motorcycle')||
+     ($totalWeight>40 && !in_array($delivery->mobility_type,['car','van']))||
+     ($totalWeight>50 && $delivery->mobility_type==='van')){
+        return response()->json(['error'=>'Cannot take this order with your current vehicle type.'],400);
+     }
     }catch(\Exception $e){
         return response()->json(['error'=> $e->getMessage()],500);
     }
