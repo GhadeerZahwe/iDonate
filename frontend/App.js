2bc01efd7b193e0fa8868681fcc4c022bce622a2
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-02 12:40:09 +0200

    Frontend: Register :tired_face:

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import Background from "./components/Background/Background";
 import Logo from "./components/Logo/logo";
-import DonorMain from "./Screens/DonorMain/DonorMain";
+import DonorMain from "./Screens/Donor/DonorMain";
 import Search from "./components/Search/Search";
 import Order from "./components/Order/Order";
 import RegisterLogo from "./components/RegisterLogo/RegisterLogo";
