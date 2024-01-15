37f1ec9e1f46144683db108e9733de4f55b5a419
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-15 19:37:36 +0200

    Backend: Delete Get Full Name from Donor Controller

--- backend/app/Http/Controllers/DonorController.php
+++ backend/app/Http/Controllers/DonorController.php
@@ -13,18 +13,6 @@ use Illuminate\Support\Facades\Auth;
 
 class DonorController extends Controller
 {
-//     public function getFullName(){
-//     $user=Auth::user();
-
-//     $first_name=$user->first_name;
-//     $last_name=$user->last_name;
-
-//     return response()->json([
-//         'first_name'=>$first_name,
-//         'last_name'=>$last_name,
-//     ]);
-//   }
-      
   public function getDonorDonations()
   {
       $donor = Auth::user();
