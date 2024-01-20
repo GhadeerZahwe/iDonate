1151362f8cd1bd8ac9209efda2fa814fc2e1d7b1
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-21 01:06:21 +0200

    Frontend: Import Some Libraries

--- frontend/Screens/Driver/OnWayLocation.js
+++ frontend/Screens/Driver/OnWayLocation.js
@@ -1,4 +1,8 @@
 import { Text } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
 const OnWayLocation = () => {
   return <Text>OnWayLocation</Text>;
