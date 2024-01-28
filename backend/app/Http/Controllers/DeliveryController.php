b78eaad041812087ffc9260ee7a94e113ecb3ca8
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-28 15:27:14 +0200

    Backend: Remove Unused Imports

--- backend/app/Http/Controllers/DeliveryController.php
+++ backend/app/Http/Controllers/DeliveryController.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\DeliveryInfo;
-use App\Models\DonorInfo;
-use App\Models\Location;
 use App\Models\User;
 use App\Models\Order;
 use Illuminate\Http\Request;
