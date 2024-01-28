dae00baff5d2167c2b6bed36d65a4307cfaaa6f3
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-28 16:06:25 +0200

    Backend: Add Check Server Status

--- backend/app/Http/Controllers/GeneralController.php
+++ backend/app/Http/Controllers/GeneralController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class GeneralController extends Controller
 {
-    //
+    public function CheckServerStatus(){
+          return response()->json([
+    'success'=>true
+  ],200);
+ }
 }
