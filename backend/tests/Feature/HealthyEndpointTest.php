345679cca10757fc02f16fd122eda70c912e289d
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-28 16:21:33 +0200

    Backend: Added Feature Test for Server Status

--- backend/tests/Feature/HealthyEndpointTest.php
+++ backend/tests/Feature/HealthyEndpointTest.php
@@ -11,10 +11,13 @@ class HealthyEndpointTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function test_server_status_endpoint(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/healthy');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+        ->assertJson([
+            'success'=> true,
+        ]);
     }
 }
