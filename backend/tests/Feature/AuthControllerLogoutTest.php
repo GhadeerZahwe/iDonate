42b1fc78b16795f6fe1f1e21445756131d067aae
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2024-01-28 19:02:23 +0200

    Backend: Add Feature Test For Logout

---
+++ backend/tests/Feature/AuthControllerLogoutTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthControllerLogoutTest extends TestCase
+{
+   
+   public function testUserLogout(){
+   $response=$this->postJson('/api/login',[
+       'email'=>'Ghadeer@gmail.com',
+       'password'=>'123456',
+   ]);
+
+   $token=$response->json('authorisation.token');
+   $response->assertStatus(200);
+   $this->assertNotNull($token);
+
+   $logoutResponse=$this->withHeaders([
+    'Authorization'=>'Bearer '. $token,
+   ])->postJson('/api/logout');
+
+   $logoutResponse->assertStatus(200)
+   ->assertJson([
+    'status'=>'success',
+    'message'=>'successfully logged out',
+   ]);
+  }
+}
